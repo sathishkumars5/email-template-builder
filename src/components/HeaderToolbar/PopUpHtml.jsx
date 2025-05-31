32ba6fa3a61362c371a79080a9627e2ed3f3672b
Mythily-29 <mythilyadckap.com>
2025-05-31 13:09:07 +0530

    Updated json

--- src/components/HeaderToolbar/PopUpHtml.jsx
+++ src/components/HeaderToolbar/PopUpHtml.jsx
@@ -8,7 +8,8 @@ function PopUpHtml({ isOpen, onClose, children }) {
       style={{
         position: 'fixed',
         top: 0, left: 0,
-        width: '100vw', height: '100vh',
+        width: '100vw',
+        height: '100vh',
         backgroundColor: 'rgba(0,0,0,0.5)',
         display: 'flex',
         justifyContent: 'center',
