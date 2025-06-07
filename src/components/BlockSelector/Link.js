69079e43e7e3db91e93e10f7ad1c3fca594fd4da
Gopi212006 <gopinathjdckap.com>
2025-06-07 18:04:17 +0530

    pull request

--- src/components/BlockSelector/Link.js
+++ src/components/BlockSelector/Link.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Link({href,id,style,linkText}) {
   return (
     <a
+          className='linkTag'
           id={id}
           style={style}
           href={href}
--- src/components/BlockSelector/NavComponents.jsx
+++ src/components/BlockSelector/NavComponents.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 import useEditorContext from '../../hooks/useEditorContext';
 import { Dragable } from './Dragable';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faRectangleList,faImage, faLink,faT,faArrowsUpDown} from '@fortawesome/free-solid-svg-icons';
+import { faRectangleList,faImage,faLink,faT,faArrowsUpDown} from '@fortawesome/free-solid-svg-icons';
 
 
 
