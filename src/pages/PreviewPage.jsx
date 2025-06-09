eeb9f89105929cab92d417ae7a4892ed80de0634
Mythily-29 <mythilyadckap.com>
2025-06-09 14:24:22 +0530

    JSON changed

--- src/pages/PreviewPage.jsx
+++ src/pages/PreviewPage.jsx
@@ -10,7 +10,7 @@ const PreviewPage = () => {
   const htmlCode = generateFullHtml(template);
 
   const handleBackToEditor = () => {
-    navigate('/');
+    navigate('/editor');
   };
 
   return (
